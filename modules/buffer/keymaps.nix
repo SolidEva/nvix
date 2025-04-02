0b7cd1efd823d76af9711a0b5062df8e9512e6a5
Eva Emmerich <eva@evaemmerich.com>
2025-04-02 17:58:16 -0500

    add bb for buffer picker and bd for buffer delete

--- modules/buffer/keymaps.nix
+++ modules/buffer/keymaps.nix
@@ -9,14 +9,14 @@ in {
 
   keymaps = [
     (mkKeymap "n" "<leader>b." "<cmd>:lua require('harpoon.mark').add_file()<cr>" "Add File to Harpoon")
-    (mkKeymap "n" "<leader>bm" "<cmd>:lua require('buffer_manager.ui').toggle_quick_menu()<cr>" "Buffer Manager")
-    (mkKeymap "n" "<leader>bb" "<cmd>:lua require('harpoon.ui').toggle_quick_menu()<cr>" "Harpoon ui")
+    (mkKeymap "n" "<leader>bb" "<cmd>:lua Snacks.picker.buffers()<cr>" "Buffer Picker")
+    (mkKeymap "n" "<leader>bm" "<cmd>:lua require('harpoon.ui').toggle_quick_menu()<cr>" "Harpoon ui")
 
     (mkKeymap "n" "<leader>bp" "<cmd>:BufferLinePick<cr>" "Buffer Line Pick")
     (mkKeymap "n" "<leader>qc" "<cmd>:bp | bd #<cr>" "Buffer close")
+    (mkKeymap "n" "<leader>bd" "<cmd>:bp | bd #<cr>" "Buffer close")
 
     (mkKeymap "n" "<leader>bP" "<cmd>BufferLineTogglePin<cr>" "Buffer Pin")
-    (mkKeymap "n" "<leader>bd" "<cmd>BufferLineSortByDirectory<cr>" "Buffer Sort by dir")
     (mkKeymap "n" "<leader>be" "<cmd>BufferLineSortByExtension<cr>" "Buffer Sort by ext")
     (mkKeymap "n" "<leader>bt" "<cmd>BufferLineSortByTabs<cr>" "Buffer Sort by Tabs")
     (mkKeymap "n" "<leader>bL" "<cmd>BufferLineCloseRight<cr>" "Buffer close all to right")
