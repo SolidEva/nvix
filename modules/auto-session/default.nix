d267493bc1e4092aebb9cc3db7806a6faed9fefd
Eva Emmerich <eva@evaemmerich.com>
2025-07-17 20:23:25 -0500

    configure auto-session to autocreate, and add default session vim opts

--- modules/auto-session/default.nix
+++ modules/auto-session/default.nix
@@ -4,9 +4,19 @@
     enable = true;
     settings = {
       auto_save = true;
-      auto_restore = false;
+      auto_restore = true;
+      auto_create = true;
+      show_auto_restore_notif = true;
     };
   };
+
+  # set plugin recomended session options
+  extraConfigLua =
+    # lua
+    ''
+      vim.o.sessionoptions="blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+    '';
+
 
   imports = with builtins; with lib;
     map (fn: ./${fn})
